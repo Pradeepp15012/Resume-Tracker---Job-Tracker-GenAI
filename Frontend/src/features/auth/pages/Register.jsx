77d2196764ba3b23ca224774d555340f80f0cc20
Real-Time-Collaborative-Whiteboard <pradeepp30956@gmail.com>
2026-03-11 08:50:04 +0530

    initial commit

---
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <main>
+      <div className="form-container">
+        <h1></h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter email address"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter password"
+            />
+          </div>
+
+          <button className="button primary-button">Login</button>
+        </form>
+      </div>
+    </main>
+  );
+}
+
+export default Register
