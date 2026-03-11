768165d3099d3035e76b4a4e3d75aeced0ddd5e0
Real-Time-Collaborative-Whiteboard <pradeepp30956@gmail.com>
2026-03-11 09:32:50 +0530

    add authentication feature

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "../auth.form.scss"
+import { useNavigate, Link } from "react-router";
 
 function Login() {
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -9,23 +11,37 @@ function Login() {
 
   return (
     <main>
-      <div className='form-container'>
+      <div className="form-container">
         <h1>Login</h1>
         <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder='Enter email address' />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter email address"
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" placeholder='Enter password' />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter password"
+            />
           </div>
 
-          <button className='button primary-button'>Login</button>
+          <button className="button primary-button">Login</button>
         </form>
+
+        <p>
+          Don't have an account? <Link to={"/register"}>Register</Link>
+        </p>
       </div>
     </main>
-  )
+  );
 }
 
 export default Login
